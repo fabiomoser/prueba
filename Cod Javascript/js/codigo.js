bd964ae71ae556e9b8aca739b1f6a5129ee692d5
fabiomoser <moser.fabio@gmail.com>
2024-10-21 21:40:46 -0300

    Sistema con JS e IA

---
+++ Cod Javascript/js/codigo.js
@@ -0,0 +1,36 @@
+/* alert('Hola mundo desde JS');
+
+let numero = -12;
+let numero2 = 2.635;
+
+numero = 50;
+numero = 'El numero es 50';
+
+let direccion = 'San Martín 1320';
+
+let estado = true; */
+
+
+// Esto es un comentario
+// var 
+/*
+const nombre = prompt('Ingrese su nombre:');  // Ejemplo: 'root'
+
+if (nombre === 'root') {
+  const apellido = prompt('Ingrese el apellido:');
+  alert(`Acceso correcto, ${nombre} ${apellido}`);
+} else {
+  alert('Acceso denegado');
+}
+
+*/
+
+// Solicita el nombre del usuario
+let nombre = prompt("¿Cuál es tu nombre?");
+
+// Verifica si el usuario ingresó algo
+if (nombre) {
+  alert(`¡Bienvenido, ${nombre}!`);
+} elif {
+  alert("¡Bienvenido, visitante!");
+}
